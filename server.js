2db8113aa9668f146bb0c905416a3687cc0e3133
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-02 09:55:35 +0530

    middleware

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const addUserMiddleware = ((req,res,next)=>{
+    req.user = 'Guest'
+    next()
+})
+
+app.get('/welcome', addUserMiddleware, (req, res)=>{
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+})
 app.listen(port,()=>{
     console.log('Server is up and running on port 3000! Ready to handle requests.')
 })
