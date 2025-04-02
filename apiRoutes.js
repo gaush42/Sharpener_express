ef5f03bc31760bb6e6ed521e9f9bb4ffda0c8b33
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-02 10:59:30 +0530

    routes and filter

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/orders',(req, res)=>{
-    res.send('Here is the list of all orders.');
+router.get('/books',(req, res)=>{
+    res.send('Here is the list of all books.');
 })
-router.post('/orders',(req, res)=>{
-    res.send('A new order has been created.');
-})
-router.get('/users',(req, res)=>{
-    res.send('Here is the list of all users.');
-})
-router.post('/users',(req, res)=>{
-    res.send('A new user has been added.');
+router.post('/books',(req, res)=>{
+    res.send('A new book has been added.');
 })
 
 module.exports = router
