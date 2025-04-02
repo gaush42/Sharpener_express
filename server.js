559f5cc5a79cfe1dddffe5918b385b8db577f9f4
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-02 10:03:27 +0530

    routes

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const addUserMiddleware = ((req,res,next)=>{
+/*const addUserMiddleware = ((req,res,next)=>{
     req.user = 'Guest'
     next()
-})
+})*/
 
-app.get('/welcome', addUserMiddleware, (req, res)=>{
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.get('/orders',(req, res)=>{
+    res.send('Here is the list of all orders.');
+})
+app.post('/orders',(req, res)=>{
+    res.send('A new order has been created.');
+})
+app.get('/users',(req, res)=>{
+    res.send('Here is the list of all users.');
+})
+app.post('/users',(req, res)=>{
+    res.send('A new user has been added.');
 })
 app.listen(port,()=>{
     console.log('Server is up and running on port 3000! Ready to handle requests.')
