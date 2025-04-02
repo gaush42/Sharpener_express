fe245674103c5ec89ced3712ad4366867e6a762c
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-02 10:29:10 +0530

    wildcard route

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const port = 3000
-
-/*const addUserMiddleware = ((req,res,next)=>{
-    req.user = 'Guest'
-    next()
-})*/
 
 app.get('/orders',(req, res)=>{
     res.send('Here is the list of all orders.');
@@ -19,6 +15,10 @@ app.get('/users',(req, res)=>{
 app.post('/users',(req, res)=>{
     res.send('A new user has been added.');
 })
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+  });
+
 app.listen(port,()=>{
     console.log('Server is up and running on port 3000! Ready to handle requests.')
 })
