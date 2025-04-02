54543941815a93974fdccac62f4bbc666eecce49
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-02 12:29:06 +0530

    ecom

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+const userRoutes = require('./routes/userRoute');
+const productRoutes = require('./routes/productRoute');
+const cartRoutes = require('./routes/cartRoute');
+
 app.use(express.json());
-const port = 3000
 
-const studentRoutes = require('./routes/studentRoutes')
-const courseRoutes = require('./routes/courseRoutes')
 
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next();
+app.get('/', (req, res) => {
+    res.send("Welcome to the E-commerce API!");
 });
 
-app.use('/students', studentRoutes)
-app.use('/courses', courseRoutes)
 
-app.get('/', (req,res) => {
-    res.send('Welcome to the Student & Course Portal API!')
-})
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
 
 app.use((req, res) => {
     res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
 
-app.listen(port,()=>{
-    console.log('Server is up and running on port 3000! Ready to handle requests.')
-})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
