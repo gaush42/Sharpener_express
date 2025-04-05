164866617e8ff5784fd38dee39423fe70d10c826
Hachiman Hikigaya <48960647+gaush42@users.noreply.github.com>
2025-04-05 10:18:55 +0530

    mysql database connection

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
 const port = 3000;
 
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'sharpener'
+})
+connection.connect((err) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log("Connection established")
+
+    const SqlQuery = `create table student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    email VARCHAR(30)
+    )`
+    connection.execute(SqlQuery, (err)=>{
+        if(err){
+            console.log(err)
+            connection.end()
+            return
+        }
+        console.log('Table created')
+    })
+})
 
 const userRoutes = require('./routes/userRoute');
 const productRoutes = require('./routes/productRoute');
